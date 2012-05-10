a1ea8c20b6dff59c37b7f7f4a040e52506074997
bob walker <bob@randomness.org.uk>
2012-05-10 17:54:21 +0000

    Made checklist thing work with tapping/clicking on address as well.

--- croydon.randomness.org.uk/static/checklist.js
+++ croydon.randomness.org.uk/static/checklist.js
@@ -8,6 +8,18 @@ $(
     $('input.checkbox').click( function() {
       set_checkbox_colour( $(this) );
     } );
+
+    $('td.checklist_address').click( function() {
+      var elem = $(this).siblings('td.checklist_status')
+                        .children('input.checkbox');
+      if ( elem.is(':checked') ) {
+        elem.removeAttr( 'checked' );
+      } else {
+        elem.attr( 'checked', 'checked' );
+      }
+      set_checkbox_colour( elem );
+    } );
+
   }
 );
 
