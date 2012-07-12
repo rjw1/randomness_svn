c124c0731c421fe37c5cc91cc61f31c8c61d9898
bob walker <bob@randomness.org.uk>
2012-07-12 21:26:22 +0000

    Reduce amount of text on node maps.

--- croydon.randomness.org.uk/static/map-leaflet.js
+++ croydon.randomness.org.uk/static/map-leaflet.js
@@ -16,7 +16,14 @@ $(
 
       var mq_url = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
       var subdomains = [ 'otile1', 'otile2', 'otile3', 'otile4' ];
-      var attrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+      var attrib;
+      // If min_lat is set, this is probably a category/locale map; if it's
+      // not, this is probably a node map (smaller hence want less text).
+      if ( min_lat ) {
+        attrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+      } else {
+        attrib = 'map tiles by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a> &amp; <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+      }
       var mapquest_layer = new L.TileLayer( mq_url, { maxZoom: 18, attribution: attrib, subdomains: subdomains } );
 
       var osm_layer = new L.TileLayer(
