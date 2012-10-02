d133f73e0aa6d57799d88be8f4ba6a3340f41b68
Kake Pugh <kake@earth.li>
2012-10-02 14:53:58 +0000

    Added sources and references to pub pages.

--- pubology.co.uk/js/pubology.js
+++ pubology.co.uk/js/pubology.js
@@ -1,3 +1,5 @@
+var sources_text;
+
 $(
   function() {
     $('#upload_msg').text('Uploading... please be patient, this may take '
@@ -27,10 +29,37 @@ $(
     if ( nav_height < win_height ) {
       $('#navbar_wrapper').height( win_height );
     }
+
+    // Hide the sources and references by default.
+    sources_text = $( '#pub_sources_and_references' ).html();
+    hide_sources();
   }
 );
 
 function reassure() {
   $('#upload_msg').show();
   return true;
+}
+
+var show_button = '(<a href="#" id="show_sources">show sources and references</a>)';
+var hide_button = '(<a href="#" id="hide_sources">hide sources and references</a>)';
+
+function hide_sources() {
+  $( '#pub_sources_and_references' ).html( show_button );
+  $( '#show_sources' ).click(
+    function() {
+      show_sources();
+      return false;
+    }
+  );
+}
+
+function show_sources() {
+  $( '#pub_sources_and_references' ).html( sources_text + hide_button );
+  $( '#hide_sources' ).click(
+    function() {
+      hide_sources();
+      return false;
+    }
+  );
 }
